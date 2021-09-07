75c698b3335891504edbbe8ff1726437589986ef
russellgoldenberg <russellgoldenberg@gmail.com>
2021-09-07 09:28:30 -0400

    update github pages

---
+++ docs/internal/pages/author/[name].svelte-a282123d.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as n}from"../../chunks/vendor-23c8bbb4.js";const o=!0;class r extends s{constructor(s){super(),t(this,s,null,null,n,{})}}export{r as default,o as prerender};
--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { ascending } from "d3";
   export let stories;
+  stories.sort((a, b) => ascending(a.hed, b.hed));
   console.log(stories[0]);
 </script>
 
 <h1>The Pudding</h1>
-{#each stories as { img }}
-  <p>{img}</p>
+{#each stories as { hed }}
+  <p>{hed}</p>
 {/each}
 <a href="/about">About</a>
