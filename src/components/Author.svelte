6ae94802a43272c17e6935aa6a261694bdb772d9
russellgoldenberg <russellgoldenberg@gmail.com>
2021-10-01 15:06:27 -0400

    author page

--- src/components/Author.svelte
+++ src/components/Author.svelte
@@ -1,10 +1,36 @@
 <script>
   export let stories;
   export let author;
+
+  $: console.log(author);
 </script>
 
-<h1>{author.name}</h1>
+<section id="info">
+  <h1>{author.name}</h1>
 
-{#each stories as { hed }}
-  <p>{hed}</p>
-{/each}
+  <img src="../common/assets/authors/{author.id}.jpg" alt={author.name} />
+
+  <p>{author.name} {@html author.bio}</p>
+
+  <ul class="links">
+    {#if author.twitter}
+      <li>
+        <a href={author.twitter}>Twitter</a>
+      </li>
+    {/if}
+
+    {#if author.url}
+      <a href={author.url}>Website</a>
+    {/if}
+  </ul>
+</section>
+
+<section id="stories">
+  <ul>
+    {#each stories as { hed, url }}
+      <li>
+        <a href="https://pudding.cool/{url}" rel="external">{hed}</a>
+      </li>
+    {/each}
+  </ul>
+</section>
