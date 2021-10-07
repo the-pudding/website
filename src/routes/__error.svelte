5280765efb917d94e48f50a8ddc3011e8c720fd9
russellgoldenberg <russellgoldenberg@gmail.com>
2021-10-07 11:24:08 -0400

    details

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,8 @@
 <script context="module">
-  export function load({ error, status }) {
+  export function load({ page, error, status }) {
     return {
       props: {
+        page,
         status,
         message: error.message
       }
@@ -10,9 +11,10 @@
 </script>
 
 <script>
+  export let page;
   export let status;
   export let message;
 </script>
 
 <h1>{status}</h1>
-<h2>{message}</h2>
+<h2>The page you are looking for, https://{page.host}{page.path} could not be found.</h2>
