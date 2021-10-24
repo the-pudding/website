c6cfd831ea6faa9edf127cae3fa5aa82264186fe
Jan Diehm <jadiehm@gmail.com>
2021-10-24 16:17:21 -0500

    promo design

--- src/components/Home.Promo.svelte
+++ src/components/Home.Promo.svelte
@@ -2,12 +2,55 @@
   export let content;
 </script>
 
-<div>
-  {@html content}
-</div>
+<a href="{content[0].link}" target="_blank">
+  <div id="promo" class="is-visible">
+    <p>{@html content[0].text}</p>
+  </div>
+</a>
 
 <style>
-  div {
-    background: var(--color-highlight);
+  #promo {
+    background: var(--color-accent);
+    border-radius: 50%;
+    width: 7rem;
+    height: 7rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 13vh;
+    right: 5vw;
+    z-index: var(--z-overlay);
+    box-shadow: 0 4px 4px var(--color-gray-light);
+    border: 2px solid var(--color-accent);
+    opacity: 0;
+    transition: opacity .25s ease-in-out;
+    pointer-events: none;
   }
+
+  #promo:hover  {
+    border: 2px solid var(--color-off-black);
+  }
+
+  #promo.is-visible {
+	  opacity: 1;
+    pointer-events: auto;
+  }
+
+  p {
+    text-align: center;
+    text-decoration: underline;
+    padding: 1rem;
+    line-height: 1.35;
+    animation: spin 10s linear .01s infinite;
+  }
+
+@keyframes spin {
+  from {
+      transform:rotate(0deg);
+  }
+  to {
+      transform:rotate(360deg);
+  }
+}
 </style>
