b12501074cc83defd60a09550d286f8735df7861
russellgoldenberg <russellgoldenberg@gmail.com>
2021-10-25 15:13:38 -0400

    404

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -11,9 +11,31 @@
 </script>
 
 <script>
-  export let page;
+  import { onMount } from "svelte";
+  import HeroText from "$components/HeroText.svelte";
   export let status;
+
+  let hsl = "hsl(0, 0%, 0%)";
+  onMount(() => {
+    setInterval(() => {
+      const h = Math.abs(Math.cos(Date.now() * 0.0001) * 256);
+      hsl = `hsl(${h}, 80%, 50%)`;
+    }, 100);
+  });
 </script>
 
-<h1>{status}</h1>
-<h2>The page you are looking for, https://{page.host}{page.path} could not be found.</h2>
+<section class="column-wide">
+  <HeroText>
+    <h1 style="color: {hsl};">THIS SITE REQUIRES FLASH</h1>
+    <p>
+      Just kidding, this is a {status}. The Pudding link you are looking for must have a typo.
+      <a href="https://pudding.cool#stories">Search all of our stories</a>, you will find it there.
+    </p>
+  </HeroText>
+</section>
+
+<style>
+  h1 {
+    transition: all 100ms ease-out;
+  }
+</style>
