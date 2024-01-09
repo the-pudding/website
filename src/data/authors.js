b30b33ca8ce22f569fe60952ad0822993fa8410e
russellsamora <russellsamora@gmail.com>
2024-01-09 10:10:41 -0500

    fix email

--- src/data/authors.js
+++ src/data/authors.js
@@ -2,15 +2,16 @@ import data from "$data/authors.csv";
 
 const makeSlug = (str) => str.toLowerCase().replace(/\//g, "_").replace(/ /g, "-");
 
-const clean = data.map(d => ({
-	id: d.id,
-	name: d.name,
-	slug: makeSlug(d.name),
-	position: d.position,
-	social: d.social,
-	url: d.url,
-	bio: d.bio,
-	pronoun: d.pronoun || "They"
+const clean = data.map((d) => ({
+  id: d.id,
+  name: d.name,
+  email: d.email,
+  slug: makeSlug(d.name),
+  position: d.position,
+  social: d.social,
+  url: d.url,
+  bio: d.bio,
+  pronoun: d.pronoun || "They"
 }));
 
 export default clean;
