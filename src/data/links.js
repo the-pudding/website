2a18c936e1af55ed7bc1edce58f6fc93b7917789
Matt Daniels <matt@polygraph.cool>
2024-09-25 10:23:17 -0400

    change subscribe links

--- src/data/links.js
+++ src/data/links.js
@@ -17,6 +17,6 @@ export const follow = [
 	{ name: "YouTube", url: "https://www.youtube.com/channel/UCFuV9vMFVluW9CAOdS4Oluw" },
 	{ name: "TikTok", url: "https://www.tiktok.com/@the_pudding" },
 	// { name: "Mastodon", url: "https://vis.social/@thepudding" },
-	{ name: "Newsletter", cta: "Join the newsletter", url: "https://thepuddingmail.substack.com" },
+	{ name: "Newsletter", cta: "Join the newsletter", url: "https://pudding.cool/subscribe" },
 	{ name: "RSS", url: "https://pudding.cool/rss.xml" }
 ];
