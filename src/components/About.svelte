b23e2411a7144b03b3a5b21327afea3ddf06f9b3
Russell Samora <russellsamora@gmail.com>
2024-10-07 09:05:58 -0400

    update about

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,7 +3,7 @@
   import HeroText from "$components/HeroText.svelte";
   import Details from "$components/Details.svelte";
   import Team from "$components/About.Team.svelte";
-  import Cohort from "$components/About.Cohort.svelte";
+  // import Cohort from "$components/About.Cohort.svelte";
   import Awards from "$components/About.Awards.svelte";
 
   export let copy;
@@ -11,7 +11,7 @@
 
   const { sections } = copy;
   const staff = authors.filter((d) => d.position === "Staff");
-  const cohort = authors.filter((d) => d.position === "2024 Cohort");
+  // const cohort = authors.filter((d) => d.position === "2024 Cohort");
 
   // hack to get contributors in details list form in copy
   const contributors = authors.filter((d) => d.position !== "Staff" && d.id !== "pudding");
@@ -53,10 +53,6 @@
 
     {#if id === "team"}
       <Team {staff} />
-    {/if}
-
-    {#if id === "team"}
-      <Cohort {cohort} />
     {/if}
 
     {#if copy[id] && copy[id].detailsSummary}
